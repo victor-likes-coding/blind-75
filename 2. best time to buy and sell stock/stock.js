4142d888486be26e7c242215b26498cd2e5cc022
Victor Tran <victor-likes-coding@tempestas.io>
2022-04-26 13:37:49 -0400

    create fn to get nums in array after lowest index

--- 2. best time to buy and sell stock/stock.js
+++ 2. best time to buy and sell stock/stock.js
@@ -10,3 +10,19 @@ const get_lowest_index = (prices = []) => {
     return lowestIndex;
 };
 
+const get_prices_larger_than_lowest_price = (prices = []) => {
+    // get lowest index or if it's the last item
+    const lowestIndex = get_lowest_index(prices);
+    const startIndex = lowestIndex + 1;
+    const pricesLength = prices.length;
+
+    // slice the prices array to that we only search for numbers after the lowest index
+    const sliceIndex = startIndex < pricesLength ? startIndex : lowestIndex;
+    const splitList = prices.slice(sliceIndex);
+    const lowestPrice = prices[lowestIndex];
+
+    // get only numbers larger than that number at that index
+    return splitList.filter((value) => {
+        return value > lowestPrice;
+    });
+};
